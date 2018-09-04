27ea70c8bba4941296336dd2fb21b17db762e5fc
Gohar Melikyan <goohar.mlkyn@gmail.com>
2018-09-04 15:46:54 +0400

    First JS_homework

---
+++ myjs.js
@@ -0,0 +1,10 @@
+alert("Hello Javascript!");
+var name = "Jon";
+var admin;
+admin = name;
+alert(admin);
+
+
+ var name = "Gohar"; surname = "Melikyan"; age = 22;
+document.write(name + /&#x223C/ + surname+ /:/ + age + /./);
+
